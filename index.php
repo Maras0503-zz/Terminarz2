ab6fd7f385ef06de323a24ae5e9429992b9ca7a6
Maras0503 <marekpiesik1@wp.pl>
2014-12-09 15:03:37 +0100

    Dodanie skryptu

--- index.php
+++ index.php
@@ -1,9 +1,4 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <?php
 
 function dni_mies($mies,$rok) {
@@ -19,8 +14,7 @@ return $dni;
 function dzien_tyg_nr($mies,$rok) {
 
  $dzien = date("w", mktime(0,0,0,$mies,1,$rok));
-
-
+ 
 return $dzien;
 }
 
@@ -43,22 +37,49 @@ return $mies_pl[$mies];
 
 <html>
 <head>
-<title>Kalendarz</title>
+    <title>Terminarz</title>
 
-<meta http-equiv="content-type" content="text/xml; charset=iso-8859-2" />
-<meta http-equiv="content-language" content="pl" />
+    <meta charset="UTF-8">
+    <meta http-equiv="content-language" content="pl" />
+    <link href="CSS/main.css" rel="stylesheet" type="text/css"/>
 
-<style type="text/css">
-#kalendarz {width: 250px;}
-#kalendarz p {text-align: right;}
-#kalendarz li {display: inline; padding:2px 5px; }
-#kalendarz .akt {color: #990000; font-weight: bold;}
-#kalendarz .hidden {visibility: hidden;}
-</style>
+    <style type="text/css">
+        #kalendarz {width:250px;}
+        #kalendarz p {text-align: right;}
+        #kalendarz li {display: inline; padding:2px 5px; }
+        #kalendarz .akt {color: #990000; font-weight: bold;}
+        #kalendarz .hidden {visibility: hidden;}
+    </style>
 </head>
 <body>
+<?php
+if(isset($_GET[imie]) && isset($_GET[nazwisko]))
+{
+    $serwer = "localhost";
+    $user = "mpadpl_admin";
+    $password = "admin123";
+    mysql_connect($serwer,$user,$password);
+    mysql_select_db("mpadpl_terminarz");
+    
+    $wpis = mysql_query("select * from klienci where email = '".$_GET[email]."'");
+    $txt = mysql_fetch_assoc($wpis);
+    $imie = ($txt["imie"]);
+    $nazwisko = ($txt["nazwisko"]);
+    $email = ($txt["email"]);
+    if ($imie != $_GET[imie] && $nazwisko != $_GET[nazwisko] && $email != $_GET[email])
+    {
+        $zapytanie = "INSERT INTO klienci (id,imie,nazwisko,email) values (null,'$_GET[imie]','$_GET[nazwisko]','$_GET[email]')" ;
+        $zapytanie = "INSERT INTO spotkania (id,termin,id_klienta,id_tematu) values (null,'$_GET[imie]','$_GET[nazwisko]','$_GET[email]')" ;
+        if(mysql_query($zapytanie))
+        {
+           echo("Umówiłeś się na spotkanie. Zapraszamy!");
+        }
+    }
+}
+?>
+<div class="mce2">
 
-<div id="kalendarz">
+<div id="kalendarz">    
 <?php
 echo '<p>'.dzien_tyg(date("w")).', '.date("d").' '.miesiac_pl(date("n")).' '.date("Y").'</p>';
 ?>
@@ -79,12 +100,56 @@ for($i=0;$i<dzien_tyg_nr(date("n"),date("Y"));$i++)
 
 for($i=1;$i<dni_mies(date("n"),date("Y")) +1;$i++) {
  if ($i<10) $i = '0'.$i;
- if ($i == date("d")) echo '<li class="akt">'.$i.'</li> ';
-  else echo '<li>'.'<button type=button>'.$i.'</button>'.'</li> ';
+ if ($i == date("d")) echo '<li>'.'<a href=index.php?dzien='.$i.' class="act">'.$i.'</a>'.'</li>';
+  else echo '<li>'.'<a href=index.php?dzien='.$i.' class="dzien">'.$i.'</a>'.'</li> ';
 }
+
 ?>
 </ul>
-<div>
-
+</div>
+</div>
+<?php
+//if(!empty($GET["dzien"]))
+{
+    echo("<br><br>Wybrałeś dzień ".$_GET["dzien"]." ".miesiac_pl(date("n"))."<br> Wybierz godzinę spotkania: <br>");
+    echo("<form></form>");
+}
+?>
+    <form action=<?php echo("index.php?dzien?godzina?imie?nazwisko?temat?email");?>>
+        <input type="hidden" value="<?php echo($_GET["dzien"]) ?>" name="dzien">
+        <select name="godzina" required>
+            <option></option>
+            <option value="1">8:00</option>
+            <option value="2">8:30</option>
+            <option value="3">9:00</option>
+            <option value="4">9:30</option>
+            <option value="5">10:00</option>
+            <option value="6">10:30</option>
+            <option value="7">11:00</option>
+            <option value="8">11:30</option>
+            <option value="9">12:00</option>
+            <option value="10">12:30</option>
+            <option value="11">13:00</option>
+            <option value="12">13:30</option>
+            <option value="13">14:00</option>
+            <option value="14">14:30</option>
+            <option value="15">15:00</option>
+        </select>
+        Temat:<select name="temat" required>
+            <option></option>
+            <option value="1">Pompy ciepła</option>
+            <option value="2">Piana poliuretanowa</option>
+            <option value="3">Prace inżynieryjne</option>
+            <option value="4">Dotacje</option>
+            <option value="5">Termowizja</option>
+            <option value="6">Wycena</option>
+            <option value="7">Reklamacja</option>
+            <option value="8">Inne</option>
+        </select>
+        Imię: <input type="text" name="imie" required>
+        Nazwisko: <input type="text" name="nazwisko" required>
+        E-mail <input type="email" name="email" required>
+        <input type="submit" value="Umów">
+    </form>
 </body>
 </html>
